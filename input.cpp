9f975a67b15d1ae2108e8a485416fdfaef5bb5dd
nhn1030 <shgusska12@gmail.com>
2023-08-22 01:16:26 +0900

    first commit

---
+++ input.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std; 
+string a;
+int main(){
+    cin >> a;
+    cout << a << "\n"; return 0;
+}
+
+
+// 대표적인 입력 함수로는 cin과 scanf 가 있음. 
+// cin은 개행문자 직전(띄어쓰기, 엔터)까지 입력을 받고, scanf는 형식을 지정해서 받음.
+
+

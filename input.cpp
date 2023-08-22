11f0630274cf04db791d0dd556dcb90cd1d5e58c
nhn1030 <shgusska12@gmail.com>
2023-08-22 16:50:22 +0900

    c++ 예제

--- input.cpp
+++ input.cpp
@@ -1,13 +1,20 @@
+// 대표적인 입력 함수로는 cin과 scanf 가 있음. 
+
+// 1. cin은 개행문자 직전(띄어쓰기, 엔터)까지 입력을 받음 아래의 코드는 다음과 같은 결과를 출력함
+
 #include <bits/stdc++.h>
 using namespace std; 
-string a;
+string a, b;
 int main(){
-    cin >> a;
-    cout << a << "\n"; return 0;
+    cin >> a >> b;
+    cout << a << "\n"; 
+    cout << b << "\n"; 
+    return 0;
 }
 
+// 입력 값 : 다들 안녕하세요
+// 실제 출력 값 : 다들
 
-// 대표적인 입력 함수로는 cin과 scanf 가 있음. 
-// cin은 개행문자 직전(띄어쓰기, 엔터)까지 입력을 받고, scanf는 형식을 지정해서 받음.
+
 
 

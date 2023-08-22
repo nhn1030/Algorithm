8bd4a953881faf217ec3bcee2c5dafa25fb68e1b
nhn1030 <shgusska12@gmail.com>
2023-08-22 19:02:54 +0900

    c++ 출력

---
+++ output.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/* 
+
+1. 출력 - cout
+일반적으로 {cout << 출력할 것 <<"\n"} 으로 사용함
+*/
+
+string a = "노현남";
+string b = "화이팅";
+
+int main() {
+    cout << a << "\n";
+    cout << a << " " << " " << b << "\n"; // 두칸 띄어쓰기
+    return 0;
+}

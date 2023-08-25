5c08ae8b3e74cec4ee38a134d1f5a38a5f01f126
nhn1030 <shgusska12@gmail.com>
2023-08-25 11:51:10 +0900

    주석 추가

--- type.cpp
+++ type.cpp
@@ -1,2 +1,23 @@
+// 알고리즘에서 자주 나오는 타입 8가지
+// void, char, string, bool, int, long long, double, unsigned long long
+
 #include <bits/stdc++.h>
 using namespace std;
+
+// 1. void -> 리턴 값이 없을 때 (java의 메서드에 들어가는 그 void 맞음 (성질은 좀 다름))
+
+int ret=1;
+
+void a(){
+    ret = 2;
+    cout << ret << "\n"; 
+    return;
+}
+
+int main() {
+    a();
+    return 0; 
+}
+
+
+

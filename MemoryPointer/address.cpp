3c39f94664ca8adcdb9f3ac5f2829e2a2e96380b
nhn1030 <shgusska12@gmail.com>
2023-09-03 19:06:07 +0900

    메모리와 포인터: 주소

---
+++ MemoryPointer/address.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int i;
+
+int main() {
+    
+    cout << &i << '\n';
+    i = 5;
+    cout << &i << '\n';
+}
+
+// & <- 엔퍼센트 연산자, 이걸 통해서 해당 객체의 주소를 가져올 수 있음.
---
+++ address.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int i;
+
+int main() {
+    cout << &i << '\n';
+}
+
+// & <- 엔퍼센트 연산자, 이걸 통해서 해당 객체의 주소를 가져올 수 있음.

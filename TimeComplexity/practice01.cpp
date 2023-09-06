737c94f4c8e07f9f2e522addf1a99cb5d28698da
nhn1030 <shgusska12@gmail.com>
2023-09-06 21:49:55 +0900

    시간복잡도 계산 연습1

---
+++ TimeComplexity/practice01.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, cnt;
+
+int main(){
+
+    cin >> n;
+    int a = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < i; j++) {
+            a += i + j;
+            cnt++;
+        }
+    }
+    cout << a << '\n';
+    cout << "cnt : " << cnt << '\n';
+    return 0;
+}
+// 디버깅을 생활화 합시다. 

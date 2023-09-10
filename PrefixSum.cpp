7124990e4b9f069047ada96adf7c13bb6f131579
nhn1030 <shgusska12@gmail.com>
2023-09-10 21:41:04 +0900

    누적합 구현하기

--- PrefixSum.cpp
+++ PrefixSum.cpp
@@ -20,5 +20,15 @@ int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     cin >> n >> m;
 
-    
+    for(int i = 0; i <= n; i++) {
+        cin >> a[i];
+        psum[i] = psum[i - 1] + a[i];
     }
+
+    for(int i = 0; i <= m; i++) {
+        cin >> b >> c;
+        cout << psum[c] - psum[b] << '\n';        
+    }
+    return 0;
+}
+

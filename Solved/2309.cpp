13e23472e1de5cd6ab0565bf264326b0ca8cbe70
nhn1030 <shgusska12@gmail.com>
2023-09-13 13:31:29 +0900

    백준 2309번 문제풀이 (퍼뮤테이션 함수를 이용한)

--- Solved/2309.cpp
+++ Solved/2309.cpp
@@ -4,4 +4,75 @@
 아홉 명의 난쟁이는 모두 자신이 "백설 공주와 일곱 난쟁이"의 주인공이라고 주장했다. 뛰어난 수학적 직관력을 가지고 있던 백설공주는, 다행스럽게도 일곱 난쟁이의 키의 합이 100이 됨을 기억해 냈다.
 
 아홉 난쟁이의 키가 주어졌을 때, 백설공주를 도와 일곱 난쟁이를 찾는 프로그램을 작성하시오.
+
+출력
+가능한 정답이 여러 가지 경우에는 아무거나 출력한다.
 */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[9];
+
+int main() {
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    for (int i = 0; i < 9; i++) {
+        cin >> a[i];
+    } 
+    sort(a, a + 9);
+
+    vector <int> result;
+
+
+    do{
+        int sum = 0;
+
+        for (int i = 0; i < 7; i++) {
+            sum += a[i];
+        }
+        
+        if (sum == 100) {
+            result.clear();
+            for (int i = 0; i < 7; i++) {
+                result.push_back(a[i]);
+            }
+            for (int i : result) {
+                cout << i << '\n';
+            }
+            cout << '\n';
+            break;
+        }
+    }while (next_permutation(a, a+9));
+
+    return 0;
+}
+
+
+    
+
+//     do{
+//         for(int i : a) cout << i << " ";
+//         cout << '\n';
+        
+//         int sum = 0;
+
+//         for (int i = 0; i < 7; i++){
+//             sum += a[i];
+//         }
+        
+//         if (sum == 100) {
+//             for (int i = 0; i < 7; i++) {
+//                 cout << a[i] << '\n';
+//             }
+//         break;
+//         }
+        
+//     }while(next_permutation(a, a + 9));
+
+//     for (int i = 7; i < 7; i++) cout << a[i] <<'\n';
+//     return 0;
+// }

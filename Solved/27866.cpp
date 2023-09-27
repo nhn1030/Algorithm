f39111892be9b4eadb28a71eb310022d845a984f
nhn1030 <shgusska12@gmail.com>
2023-09-28 00:04:54 +0900

    백준 27866번 솔브

--- Solved/27866.cpp
+++ Solved/27866.cpp
@@ -1,3 +1,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int main() {
+    int idx;
+    string s;
+
+    cin >> s >> idx;
+
+    cout << s[idx - 1];
+
+    return 0;
+}
+
+// 알게된점 c++도 문자열 인덱싱을 지원한다.

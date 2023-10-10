e78804a4ea3996bec741bb0ed61e33ffdcf881fb
nhn1030 <shgusska12@gmail.com>
2023-10-10 22:44:13 +0900

    9996번 내방식으로 풀어보기

--- Solved/9996.cpp
+++ Solved/9996.cpp
@@ -7,9 +7,9 @@ string s, ori_s, pre, suf; // 문자열 s, ori_s는 패턴을 뜻하며, pre는 
 int main() {
     cin >> n; // 파일의 갯수 n개를 입력
     cin >> ori_s; // 어떠한 패턴을 사용할 것인지 입력 ex ) a*b, aa*bb, aa*cc, 등
-
     int pos = ori_s.find('*'); // find()는 특정 문자열을 찾아 위치를 반환하는 함수임 참고 pos는 문자열의 현 위치를 나타내는 변수로 선언됨
-    pre = ori_s.substr(0, pos + 1); // pre는 prefix(접두사) ori_s 문자열의 0번째부터 pos위치까지를 반환하여 저장
+    
+    pre = ori_s.substr(0, pos); // pre는 prefix(접두사) ori_s 문자열의 0번째부터 pos위치까지를 반환하여 저장
     suf = ori_s.substr(pos + 1); // suf는 suffix(접미사) ori_s 문자열의 pos + 1의 위치 부터 문자열의 끝까지를 반환하여 저장 (시작 지점만 넣게 되면 마지막까지 뽑아냄)
 
     for(int i = 0; i < n; i++) { // 파일을 얻기 위해 n회 만큼 반복한다.
@@ -20,7 +20,7 @@ int main() {
         else{
             if(pre == s.substr(0, pre.size()) && suf == s.substr(s.size() - suf.size())) cout << "DA\n"; // 문자열 s의 접두사가 pre와 일치하고 suf와 s의 접미사가 일치하는지 확인, 맞다면 DA출력 
             else cout << "NE\n"; // 그 외의 경우 NE를 출력.
-            }
+        }
     
     }
     
---
+++ Solved/new9996.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    int n;
+    string s, pattern, pre, suf;
+
+    cin >> n;
+    cin >> pattern;
+
+    int pos = pattern.find("*");
+    pre = pattern.substr(0, pos);
+    suf = pattern.substr(pos + 1);
+
+
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+
+        if (s.size() < pre.size() + suf.size()) {
+            cout << "NE\n";
+        }
+        else {
+            bool match = true; // 문자열과 패턴이 일치하는 지 검사
+
+            for (int j = 0; j < pre.size(); j++) {
+                if(tolower(pre[j]) != tolower(s.size())) {
+                    match = false;
+                    break;
+                }
+            }
+
+            for (int j = 0; j < suf.size(); j++) {
+                if(tolower(suf[j]) != tolower(s[s.size() - suf.size() + j])) {
+                    match = false;
+                    break;
+                }
+            }
+        }
+    }
+
+
+
+}
